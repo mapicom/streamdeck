16d150ad4560ec94f8643ae7532f1a632936ca31
Maksim Pinigin <at@pinig.in>
2025-11-15 17:53:07 +0700

    Add iOS support

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -1,9 +1,15 @@
 import { Preferences } from '@capacitor/preferences';
 import { App } from '@capacitor/app';
+import { Device } from '@capacitor/device';
 
 document.body.onload = async (event) => {
     const acceptBtnElem = document.getElementById("accept-btn");
     const quitBtnElem = document.getElementById("quit-btn");
+
+    const devInfo = await Device.getInfo();
+    if(devInfo.platform !== "android") {
+        quitBtnElem.remove();
+    }
 
     acceptBtnElem.onclick = async (event) => {
         await Preferences.set({
