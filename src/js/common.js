b6006a49d0a9637d1a5316ca7d8b71d009fe8bfd
Maksim Pinigin <at@pinig.in>
2026-01-08 15:54:54 +0700

    Add copyright notice to `src/js/common.js`

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,5 @@
+// Copyright (c) Maksim Pinigin <at@pinig.in>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
 
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { App } from '@capacitor/app';
